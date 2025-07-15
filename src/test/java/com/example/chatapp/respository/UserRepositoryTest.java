94cf0d01ac7fb41b9d9bc5fe9cb59ec3fbba611c
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-07-15 21:10:58 +0100

    make user model

--- src/test/java/com/example/chatapp/respository/UserRepositoryTest.java
+++ src/test/java/com/example/chatapp/respository/UserRepositoryTest.java
@@ -4,9 +4,12 @@ import com.example.chatapp.model.User;
 import com.example.chatapp.model.UserStatus;
 import com.example.chatapp.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.UUID;
@@ -14,7 +17,6 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataMongoTest
-@Import(UserRepository.class)
 public class UserRepositoryTest {
 
     @Autowired
@@ -22,6 +24,15 @@ public class UserRepositoryTest {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+    }
+
 
     @Test
     public void testSaveUser() {
