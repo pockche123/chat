5348c29d147c5213059ed17daebb42e9542d5934
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-07-15 21:19:09 +0100

    add findByUsername to the repo

--- src/main/java/com/example/chatapp/repository/UserRepository.java
+++ src/main/java/com/example/chatapp/repository/UserRepository.java
@@ -4,8 +4,11 @@ import com.example.chatapp.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
 }
--- src/test/java/com/example/chatapp/respository/UserRepositoryTest.java
+++ src/test/java/com/example/chatapp/respository/UserRepositoryTest.java
@@ -3,15 +3,17 @@ package com.example.chatapp.respository;
 import com.example.chatapp.model.User;
 import com.example.chatapp.model.UserStatus;
 import com.example.chatapp.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,6 +26,15 @@ public class UserRepositoryTest {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @AfterEach
+    public void tearDown() {
+        mongoTemplate.dropCollection(User.class);
+    }
+
 
     @TestConfiguration
     static class TestConfig {
@@ -46,6 +57,27 @@ public class UserRepositoryTest {
 
         assertNotNull(savedUser.getUserId());
         assertEquals("testUser", savedUser.getUsername());
+        assertEquals(UserStatus.ONLINE, savedUser.getUserStatus());
+        assertTrue(passwordEncoder.matches("testPassword", savedUser.getPassword()));
+    }
+
+    @Test
+    public void test_findBYUsername_getUser(){
+        User user = new User();
+        UUID userId = UUID.randomUUID();
+        user.setUserId(userId);
+        user.setUsername("testUser");
+        user.setPassword(passwordEncoder.encode("testPassword"));
+        user.setUserStatus(UserStatus.ONLINE);
+        userRepository.save(user);
+
+        Optional<User> optionalUser = userRepository.findByUsername("testUser");
+        User savedUser = optionalUser.get();
+
+
+        assertNotNull(savedUser.getUserId());
+        assertEquals("testUser", savedUser.getUsername());
+        assertEquals(UserStatus.ONLINE, savedUser.getUserStatus());
         assertTrue(passwordEncoder.matches("testPassword", savedUser.getPassword()));
     }
 }
