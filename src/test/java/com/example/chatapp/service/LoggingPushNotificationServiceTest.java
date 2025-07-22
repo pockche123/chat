9c029cf3b180edb24f1adb89ffc994a7ad5e0ee6
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-07-22 16:52:43 +0100

    test loggingpushnotificationservice

--- src/test/java/com/example/chatapp/service/LoggingPushNotificationServiceTest.java
+++ src/test/java/com/example/chatapp/service/LoggingPushNotificationServiceTest.java
@@ -1,9 +1,28 @@
 package com.example.chatapp.service;
 
+import com.example.chatapp.model.ChatMessage;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @ExtendWith(MockitoExtension.class)
 public class LoggingPushNotificationServiceTest {
+
+    private LoggingPushNotificationService pushNotificationService = new LoggingPushNotificationService();
+
+    @Test
+    void sendNotification_shouldNotThrowException() {
+        // Given
+
+        ChatMessage message = new ChatMessage();
+        message.setMessageId(UUID.randomUUID());
+
+        // When/Then - Just verify it doesn't throw
+        assertDoesNotThrow(() -> pushNotificationService.sendNotification(message));
+    }
 
 }
