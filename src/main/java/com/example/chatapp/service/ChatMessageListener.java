aa6e335ceb7cecae8ba26c2b84fd0f1766726577
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-07-23 15:12:32 +0100

    finish routing

--- src/main/java/com/example/chatapp/service/ChatMessageListener.java
+++ src/main/java/com/example/chatapp/service/ChatMessageListener.java
@@ -2,6 +2,7 @@ package com.example.chatapp.service;
 
 import com.example.chatapp.event.ChatMessageEvent;
 import com.example.chatapp.model.ChatMessage;
+import com.example.chatapp.repository.ChatMessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -17,14 +18,24 @@ public class ChatMessageListener {
 
     @Autowired
     private PushNotificationService pushNotificationService;
+    
+    @Autowired
+    private ChatMessageRepository chatMessageRepository;
 
     @EventListener
     public void handleChatMessage(ChatMessageEvent event){
         ChatMessage message = event.getMessage();
-            if(onlineUserService.isUserOnline(message.getReceiverId())){
-                messageDeliveryService.deliverMessage(message);
-            } else{
-                pushNotificationService.sendNotification(message);
-            }
+        
+        // Always store the message regardless of user's online status
+        chatMessageRepository.save(message).subscribe();
+        
+        if(onlineUserService.isUserOnline(message.getReceiverId())){
+            // Deliver immediately if user is online
+            messageDeliveryService.deliverMessage(message);
+        } else {
+            // Send push notification if user is offline
+            pushNotificationService.sendNotification(message);
+            // Message is already stored and will be delivered when user comes online
+        }
     }
 }
