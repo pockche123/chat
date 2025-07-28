b3f6df7496ad20a2fdddc5ccad05224aebd738a7
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-07-28 17:00:22 +0100

    add a new query in chatMesagerepository

--- src/main/java/com/example/chatapp/repository/ChatMessageRepository.java
+++ src/main/java/com/example/chatapp/repository/ChatMessageRepository.java
@@ -1,6 +1,7 @@
 package com.example.chatapp.repository;
 
 import com.example.chatapp.model.ChatMessage;
+import com.example.chatapp.model.MessageStatus;
 import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -18,4 +19,7 @@ public interface ChatMessageRepository extends ReactiveCassandraRepository<ChatM
     Flux<ChatMessage> findByReceiverId(UUID receiverId);
     @Query("SELECT * FROM chat_messages WHERE receiver_id = ?0 AND status = ?1 ALLOW FILTERING")
     Flux<ChatMessage> findByReceiverIdAndStatus(UUID receiverId, String status);
+
+    @Query("SELECT * FROM chat_message WHERe conversation_id = ?0 AND receiver_id = ?1 AND status = ?2 ALLOW FILTERING")
+    Flux<ChatMessage> findByConversationIdAndReceiverIdAndStatus(UUID conversationId, UUID receiverId, MessageStatus status);
 }
