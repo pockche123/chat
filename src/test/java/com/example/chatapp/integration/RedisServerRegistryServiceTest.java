ac722e60939b515409180efa87c02b67a092f190
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-08-06 17:16:50 +0100

    refacor redisserverregistryservicetest

--- src/test/java/com/example/chatapp/integration/RedisServerRegistryServiceTest.java
+++ src/test/java/com/example/chatapp/integration/RedisServerRegistryServiceTest.java
@@ -4,10 +4,6 @@ import com.example.chatapp.service.RedisServerRegistryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import reactor.test.StepVerifier;
 
@@ -15,19 +11,7 @@ import java.util.UUID;
 
 @SpringBootTest
 @Testcontainers
-public class RedisServerRegistryServiceTest {
-
-
-    @Container
-    static GenericContainer<?> redis = new GenericContainer<>("redis:alpine")
-            .withExposedPorts(6379);
-
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.redis.host", redis::getHost);
-        registry.add("spring.data.redis.port", redis::getFirstMappedPort);
-    }
+public class RedisServerRegistryServiceTest extends BaseIntegrationTest{
 
     @Autowired
     private RedisServerRegistryService serverRegistryService;
