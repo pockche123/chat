467fd19a969c9bc56c1b680c6208e221b0015a65
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-10-01 12:25:53 +0100

    add FirebaseConfig

--- src/main/java/com/example/chatapp/model/UserDevice.java
+++ src/main/java/com/example/chatapp/model/UserDevice.java
@@ -3,6 +3,7 @@ package com.example.chatapp.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 import java.util.UUID;
 
@@ -10,6 +11,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDevice {
+    @Id
     private String deviceToken;
     private String deviceType;
     private UUID userId;
