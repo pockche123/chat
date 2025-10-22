2c63b00f886a76ab9484bd9d7d3ba26f11b56b01
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-10-22 15:01:28 +0100

    create service layer for creating and updating group

--- src/main/java/com/example/chatapp/dto/GroupDTO.java
+++ src/main/java/com/example/chatapp/dto/GroupDTO.java
@@ -1,29 +1,21 @@
-package com.example.chatapp.model;
+package com.example.chatapp.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-@Document("groups")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Group {
-    @Id
+public class GroupDTO {
     private UUID conversationId;
-    private List<UUID> memberIds = new ArrayList<>();
+    private List<UUID> memberIds;
     private String name;
     private UUID adminId;
-    private Timestamp createdAt;
-
-
 }
